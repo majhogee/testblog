a309f38ffaba6bf82a3e11993b26bcb64c674dbd
majhogee <jo.wojtowicz@gmail.com>
2014-02-08 11:49:25 +0100

    Changing field content to body in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,10 @@ class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :content, type: String
+  field :body, type: String
 
   belongs_to :post
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  has_many :votes
+  validates :body, length: { maximum: 140 }
 end
